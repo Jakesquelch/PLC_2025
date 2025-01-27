ce04ba74bb00c1276e6c35d6b48021aeccce9d2d
Jake Squelch <jakewsquelch@gmail.com>
2025-01-27 18:55:23 +0000

    Wrote better function for given range

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,9 +1,24 @@
---create inpFunc
-inpFunc = [1..5] 
+-- Define inpFunc
+inpFunc :: Int -> Int -> [Int] --Dont need this just good to have
+inpFunc start end = [start..end] 
 
---Define applicatorFunc
-applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
+-- Define applicatorFunc
+applicatorFunc :: [Int] -> Char -> Double --Dont need this just good to have
+applicatorFunc inpFunc s = 
+  if s == 's' 
+    then fromIntegral (sum inpFunc) 
+    else fromIntegral (sum inpFunc) / fromIntegral (length inpFunc)
 
+main :: IO ()
 main = do
-    let result = applicatorFunc inpFunc 'a' --Call applicatorFunc with inpFunc and 'a' as args
-    putStrLn("sum = " ++ show(result))
+    putStrLn "Enter the start of the range:"
+    start <- readLn :: IO Int
+    putStrLn "Enter the end of the range:"
+    end <- readLn :: IO Int
+
+    if start > 0 && end >= start 
+      then do
+        let result = applicatorFunc (inpFunc start end) 'a' -- Call applicatorFunc with inpFunc and 'a'
+        putStrLn ("Result = " ++ show result)
+      else
+        putStrLn "Please enter valid positive integers with start <= end."

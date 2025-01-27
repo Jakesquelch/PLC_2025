d862bd19b116d6b49a81522481f7054f93cfaaa7
Jake Squelch <jakewsquelch@gmail.com>
2025-01-27 14:50:10 +0000

    Updated an error within Haskell code

--- lab2-functionalprimer/haskell/intro1.hs
+++ lab2-functionalprimer/haskell/intro1.hs
@@ -2,7 +2,7 @@ module Intro1 where
 
 -- define named constants:
 r1 = 1
-r2 = b^2 + 1/b where b = 2
+r2 = b^2 + 1/b where b = 2.0 -- Added a decimal to make it a float
 
 -- define a function:
 diff a b = abs (a - b)

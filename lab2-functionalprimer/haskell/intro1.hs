c4692ae6791f8facee3ce5fc5656f3a2831436e0
Jake Squelch <jakewsquelch@gmail.com>
2025-01-27 14:59:53 +0000

    Another correction

--- lab2-functionalprimer/haskell/intro1.hs
+++ lab2-functionalprimer/haskell/intro1.hs
@@ -8,7 +8,9 @@ r2 = b^2 + 1/b where b = 2.0 -- Added a decimal to make it a float
 diff a b = abs (a - b)
 
 r3 = diff (diff a a) a  where a = 1
-r4 = diff (diff a b) b  where a = 1; b = 1
+r4 = diff (diff a b) b  where
+    a = 1 -- removed semi-coloned line and separated line for readability
+    b = 1
 
 -- lists by enumeration
 r5 = [1,3,4,1]

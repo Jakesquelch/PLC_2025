814ad2d28b561baf58f083727f8e434ab0567a0a
Jake Squelch <jakewsquelch@gmail.com>
2025-01-27 19:18:02 +0000

    Completed the final passing functions as argument, this was js, previously done haskell then python

--- lab2-functionalprimer/javascript/func_as_arg.js
+++ lab2-functionalprimer/javascript/func_as_arg.js
@@ -1,24 +1,44 @@
-//Create list of ints from 1 to 5, Haskell equivalent [1..5]
-function arrFunc(){
+// Create a list of integers from `a` to `b`, Haskell equivalent [a..b]
+function arrFunc(a, b) {
     let arr = [];
-    for (let i = 1; i<=5; i++) {
+    for (let i = a; i <= b; i++) {
         arr.push(i);
     }
-    return arr;    
+    return arr;
 }
 
-function applicatorFunc(inpFunc, s){
-    if(s=='s'){
-        const arr = inpFunc();        
-        let sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-        return sum;
-    }
-    else{        
-        const arr = inpFunc();
-        let sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-        return sum/5;
+// Generalized applicator function to calculate sum or average
+function applicatorFunc(inpFunc, a, b, s) {
+    const arr = inpFunc(a, b); // Generate the list dynamically
+    let sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    if (s === 's') {
+        return sum; // Return the sum
+    } else {
+        return sum / arr.length; // Return the average
     }
 }
 
-let x = applicatorFunc(arrFunc, 's');
-console.log(x);
+// Using Node's readline module to take input from the command line
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter the starting number (a): ', (a) => {
+    rl.question('Enter the ending number (b): ', (b) => {
+        rl.question("Enter 's' for sum or any other key for average: ", (operation) => {
+            // Convert input values to integers
+            a = parseInt(a);
+            b = parseInt(b);
+
+            // Compute the result using the applicator function
+            let result = applicatorFunc(arrFunc, a, b, operation);
+            console.log(`Result: ${result}`);
+
+            // Close the readline interface
+            rl.close();
+        });
+    });
+});
